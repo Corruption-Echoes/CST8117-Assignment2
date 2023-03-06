e55ebb0da2cf7e529e898f0914a61e36d16c5301
Aric <morr0457@gmail.com>
2023-03-06 16:43:53 -0500

    Tentative bubble code

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
+const bubbles = []
+const bubbleSpawners = document.getElementsByClassName('BubbleSpawnerDivs')
+
+function createBubble () {
+  const spawn = pickSpawner()
+  bubbles.push({ x: spawn.left, y: 0 })
+}
 
 function bubbleWander (bubble) {
-  bubble.style.bottom = bubble.style.bottom + 1
+  bubble.style.bottom = bubble.y + 1
+  bubble.y += 1
 }
+
+function pickSpawner () {
+  return bubbleSpawners.random()
+}
+
+function animate () {
+  for (const b in bubbles) {
+    bubbleWander(b)
+  }
+}
+
+setInterval(animate, 150)
