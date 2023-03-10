8908bcd7d55d4963157f1b36fd18ef2492ed9f52
Aric <morr0457@gmail.com>
2023-03-10 12:37:42 -0500

    Added personal score logic

--- javascript/highscore.js
+++ javascript/highscore.js
@@ -1,21 +1,30 @@
 const highscoreContainer = document.getElementById('highscoreContainer')
+const personalContainer = document.getElementById('personalConstainer')
+let ranking = 0
+
 function retrieveHighscores () {
   const leaderboardData = APIGOESHERE()
-
   const table = document.createElement('table')
   for (const score of leaderboardData) {
-    table.appendChild(printToBoard(score))
+    ranking++
+    table.appendChild(printToBoard(score, ranking))
+    if (score.name == login.name) {
+      personalContainer.textContent = ' #' + ranking + ' ' + score.score
+    }
   }
   highscoreContainer.appendChild(table)
 }
-function printToBoard (score) {
+function printToBoard (score, ranking) {
   const row = document.createElement('tr')
+  const rankingTD = document.createElement('td')
   const nameTD = document.createElement('td')
   const scoreTD = document.createElement('td')
   const timeTD = document.createElement('td')
+  rankingTD.textContent = ranking
   nameTD.textContent = score.name
   scoreTD.textContent = score.score
   timeTD.textContent = score.time
+  row.appendChild(rankingTD)
   row.appendChild(nameTD)
   row.appendChild(scoreTD)
   row.appendChild(timeTD)
