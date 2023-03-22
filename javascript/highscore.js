7ffae6f8acac02734ba725ec06f5b74fe005c32e
Aric <a.morrow44@gmail.com>
2023-03-22 11:52:47 -0400

    loading for login

--- javascript/highscore.js
+++ javascript/highscore.js
@@ -1,15 +1,20 @@
 
 const highscoreContainer = document.getElementById('highscoreContainer')
 const personalContainer = document.getElementById('personalConstainer')
-let ranking = 0
-//const login = { username: 'waffle' }
+
+const login = localStorage.getItem("userID")
 
 function printHighscores (leaderboardData) {
   //console.log('Data Received:' + leaderboardData)
   highscoreContainer.innerHTML=leaderboardData
 }
+function printPersonalHighscores(leaderboardData){
+  personalContainer.innerHTML=leaderboardData
+}
 function loadHighscores(){
   getHighscores(printHighscores)
+  if(login!==null)
+  getHighscore(login,printPersonalHighscores)
 }
 
 loadHighscores()
