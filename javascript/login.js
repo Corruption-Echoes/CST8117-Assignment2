6fb9383ec2172ceb44f7d9ac50e9d7b34f066963
Aric <morr0457@algonquinlive.com>
2023-03-24 13:45:19 -0400

    login data v1

--- javascript/login.js
+++ javascript/login.js
@@ -2,9 +2,12 @@ const userInput = document.getElementById('username')
 const passwordInput = document.getElementById('password')
 
 function login () {
-
+  attemptLogin(userInput.value, passwordInput.value, loginResponseHandler)
 }
 function register () {
-  const address = '../registration.html?u=' + userInput.value + '&p=' + passwordInput.value
+  const address = 'registration.html?u=' + userInput.value + '&p=' + passwordInput.value
   window.location.href = address
+}
+function loginResponseHandler (data) {
+  console.log(data)
 }
